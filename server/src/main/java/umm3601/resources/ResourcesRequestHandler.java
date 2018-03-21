1c17073c8bfc94b9ab643df309c5adc94d546c56
jubairmr9 <yasin011@morris.umn.edu>
2018-03-21 15:28:29 -0500

    added template for controller and handler

---
+++ server/src/main/java/umm3601/resources/ResourcesRequestHandler.java
@@ -0,0 +1,21 @@
+
+package umm3601.resources;
+
+    import com.google.gson.Gson;
+    import com.mongodb.*;
+    import com.mongodb.client.FindIterable;
+    import com.mongodb.client.MongoCollection;
+    import com.mongodb.client.MongoDatabase;
+    import com.mongodb.util.JSON;
+    import org.bson.Document;
+    import org.bson.types.ObjectId;
+    import java.util.Iterator;
+    import java.util.Map;
+
+
+    import java.util.Date;
+
+    import static com.mongodb.client.model.Filters.eq;
+
+public class ResourcesRequestHandler {
+}
