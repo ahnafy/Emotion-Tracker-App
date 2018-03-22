fc5f07af52d093df14064a0bc40c7a40ec2a8c56
jubairmr9 <yasin011@morris.umn.edu>
2018-03-22 15:27:39 -0500

    added resources component and service component #11, #3

--- client/src/app/resources/resources.service.ts
+++ client/src/app/resources/resources.service.ts
@@ -1,4 +1,3 @@
-//Currently doesn't do
 
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
@@ -10,9 +9,36 @@ import {environment} from '../../environments/environment';
 import {ResourcesComponent} from "./resources.component";
 
 
+
 @Injectable()
 export class ResourcesService {
     readonly baseUrl: string = environment.API_URL + 'resources';
-    private resourceUrl: string = this.baseUrl;
+    private resourcesUrl: string = this.baseUrl;
+
+    constructor(private http: HttpClient) {
+    }
+// Resources over resources **SIDE NOTE FOR Ahnaf, if things break its for that
+
+    addResources(newResources: resources): Observable<{'$oid': string}> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+        };
+        // Send post request to add a new name with the resource data as the body with specified headers.
+
+        return this.http.post<{'$oid': string}>(this.resourcesUrl + '/new', newResources, httpOptions);
+    }
+
+    getResourcesById(id: string): Observable<resources> {
+        return this.http.get<resources>(this.resourcesUrl + '/' + id);
+    }
+    getResources(resourcesName?: string): Observable<resources[]> {
+        if(resourcesName) {
+            return this.http.get<resources[]>(this.resourcesUrl + '?name=' + resourcesName);
+        }
+        return this.http.get<resources[]>(this.resourcesUrl);
+    }
+
 
 }
