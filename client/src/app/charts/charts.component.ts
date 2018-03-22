77c43ff30e4672801d71f3f0a1a668b5812c7a0a
blake376 <blake376@morris.umn.edu>
2018-03-22 15:29:25 -0500

    trying to get filterWeek working

--- client/src/app/charts/charts.component.ts
+++ client/src/app/charts/charts.component.ts
@@ -14,6 +14,11 @@ export class ChartsComponent implements OnInit {
     public emojis: Emoji[];
     public filteredEmojis: Emoji[];
 
+    startDate;
+    endDate;
+    getDate;
+
+
     // These are the target values used in searching.
     // We should rename them to make that clearer.
     public emojiOwner: string;
@@ -23,8 +28,42 @@ export class ChartsComponent implements OnInit {
     constructor(public reportsService: ReportsService) {
 
     }
+/*
+    public filterDates(searchMood: string, searchStartDate: any, searchEndDate: any): Summary[] {
 
+        this.filteredSummarys = this.summarys;
 
+        // Filter by Mood
+        if (searchMood != null) {
+            searchMood = searchMood.toLocaleLowerCase();
+
+            this.filteredSummarys = this.filteredSummarys.filter(summary => {
+                return !searchMood || summary.mood.toLowerCase().indexOf(searchMood) !== -1;
+            });
+        }
+
+        // Filter by startDate
+        if (searchStartDate != null) {
+
+            this.filteredSummarys = this.filteredSummarys.filter(summary => {
+                this.getDate = new Date(summary.date);
+                return this.getDate >= this.startDate;
+            });
+        }
+
+        // Filter by endDate
+        if (searchEndDate != null) {
+
+            this.filteredSummarys = this.filteredSummarys.filter(summary => {
+                this.getDate = new Date(summary.date);
+                return this.getDate <= this.endDate;
+            });
+        }
+
+        return this.filteredSummarys;
+    }
+    */
+/*
     public filterEmojis(searchOwner): Emoji[] {
 
         this.filteredEmojis = this.emojis;
@@ -45,32 +84,67 @@ export class ChartsComponent implements OnInit {
             return date2.valueOf() - date1.valueOf();
         });
 
+
+        return this.filteredEmojis;
+    } */
+
+    //get current date... good for debug
+
+    getdate(): string{
+        return Date();
+    }
+
+    public filterWeek(searchStartDate, searchEndDate): Emoji[] {
+        this.filteredEmojis = this.emojis;
+
+        //for testing purposes, manually setting start and end date
+        this.startDate = "Sun Mar 18 2018 10:00:00 GMT-0500 (CDT)";
+        this.endDate = "Sat Mar 24 2018 20:00:00 GMT-0500 (CDT)";
+
+        // Filter by startDate
+        if (searchStartDate != null) {
+
+            this.filteredEmojis = this.filteredEmojis.filter(emoji => {
+                this.getDate = new Date(emoji.date);
+                return this.getDate >= this.startDate;
+            });
+        }
+
+        // Filter by endDate
+        if (searchEndDate != null) {
+
+            this.filteredEmojis = this.filteredEmojis.filter(emoji => {
+                this.getDate = new Date(emoji.date);
+                return this.getDate <= this.endDate;
+            });
+        }
         return this.filteredEmojis;
     }
 
-    public numberByDay(weekday): number {
 
-        this.filteredEmojis = this.emojis;
+    public filterMood(mood, partiallyFilteredEmotions): Emoji[] {
 
-        // Filter by day of the week
-            this.filteredEmojis = this.filteredEmojis.filter(emoji => {
-                return !weekday || emoji.date.indexOf(weekday) !== -1;
-            });
+        this.filteredEmojis = partiallyFilteredEmotions;
 
-        return this.filteredEmojis.length;
-    }
-
-    public filterMood(mood): Emoji[] {
-
-        this.filteredEmojis = this.emojis;
 
         // Filter by value
         this.filteredEmojis = this.filteredEmojis.filter(emoji => {
-            return !mood || emoji.mood !== -1;//??????
+            return !mood.toString() || emoji.mood.toString().indexOf(mood.toString()) !== -1;//??????
         });
 
 
         return this.filteredEmojis;
+    }
+    public numberByDay(weekday, partiallyFilteredEmotions): number {
+
+        this.filteredEmojis = partiallyFilteredEmotions;
+
+        // Filter by day of the week
+        this.filteredEmojis = this.filteredEmojis.filter(emoji => {
+            return !weekday || emoji.date.indexOf(weekday) !== -1;
+        });
+
+        return this.filteredEmojis.length;
     }
 
 
@@ -78,6 +152,7 @@ export class ChartsComponent implements OnInit {
      * Starts an asynchronous operation to update the emojis list
      *
      */
+
     refreshEmojis(): Observable<Emoji[]> {
         // Get Emojis returns an Observable, basically a "promise" that
         // we will get the data from the server.
@@ -89,7 +164,7 @@ export class ChartsComponent implements OnInit {
         emojiListObservable.subscribe(
             emojis => {
                 this.emojis = emojis;
-                this.filterEmojis(this.emojiOwner);
+                //this.filterEmojis(this.emojiOwner);
             },
             err => {
                 console.log(err);
@@ -98,7 +173,9 @@ export class ChartsComponent implements OnInit {
     }
 
 
+
     ngOnInit(): void {
         this.refreshEmojis();
     }
+
 }
